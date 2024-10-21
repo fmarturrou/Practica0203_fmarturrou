e924128c0ac80ac4d089ffbdd0fc998f3aa40828
fmarturrou <fmarturrou1@educacion.navarra.es>
2024-10-21 12:00:06 +0200

    Ejercicio04

---
+++ Ejercicio04.py
@@ -0,0 +1,9 @@
+edad = int(input("¿Cúal es tu edad?\n"))
+if edad < 16:
+    print("No tienes la edad minima para trabajar por lo que no puedes tributar.")
+ingresos = int(input("¿Cúal es tu salario bruto mensual?\n"))
+if edad >= 16:
+    if ingresos >= 1000:
+        print("Según los datos introducidos, debes tributar.")
+if ingresos < 1000:
+    print("No recibes los ingresos mínimos para tributar")
